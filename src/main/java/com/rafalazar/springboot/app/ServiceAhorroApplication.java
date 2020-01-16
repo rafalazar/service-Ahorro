a0ee6935252ad5cc3357620aa8a0d811421e887c
Jordan <jcr.salazar@outlook.com>
2020-01-15 20:37:44 -0500

    Microservicio de cuenta de Ahorro creada.

---
+++ src/main/java/com/rafalazar/springboot/app/ServiceAhorroApplication.java
@@ -0,0 +1,13 @@
+package com.rafalazar.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAhorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAhorroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafalazar/springboot/app/ServiceAhorroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafalazar.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAhorroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
