87f93f32e62d6e18ae61772d7ec8328f7d0aaac9
Jordan <jcr.salazar@outlook.com>
2020-01-21 16:36:58 -0500

    Se han agregado las configuraciones necesarias para Eureka y Swagger.

--- src/main/java/com/rafalazar/springboot/app/ServiceAhorroApplication.java
+++ src/main/java/com/rafalazar/springboot/app/ServiceAhorroApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.rafalazar.springboot.app.documents.AhorroAccount;
@@ -15,6 +16,7 @@ import com.rafalazar.springboot.app.service.AhorroAccountService;
 
 import reactor.core.publisher.Flux;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceAhorroApplication implements CommandLineRunner{
 	
